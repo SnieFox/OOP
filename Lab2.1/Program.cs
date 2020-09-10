676de9e0ccd8ac68fd9d938e2e65771a616f8a33
SnieFox <legueoflegend30@gmail.com>
2020-09-10 22:12:44 +0300

    Tasks 1-2

---
+++ Lab2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\tWidth: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Lab2.1/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._1
+{
+    class Rectangle
+    {
+        public double side1 { get; set; }
+        public double side2 { get; set; }
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public double AreaCalculator()
+        {
+            double area = side1 * side2;
+            return area;
+        }
+        public double PerimeterCalculator()
+        {
+            double perim = (side1 + side2) * 2;
+            return perim;
+        }
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+            private set
+            {
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+            private set
+            {
+            }
+        }
+
+    }
+}
